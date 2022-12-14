6ad4b5888955eef67afbc6b6ae79f3b581dc3a5e
AlenaGM <alena.guillaume4@gmail.com>
2022-12-14 16:35:54 +0100

    project structure setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import LayoutHeader from "@/components/Layout/Header.vue";
+import LayoutSidebar from "@/components/Layout/Sidebar.vue";
+import { ref } from "vue";
+const isOpenMenu = ref(false);
+const toggleMenu = () => {
+  isOpenMenu.value = !isOpenMenu.value;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="sidebar-toggle" @click="toggleMenu">&#5125;</div>
+    <layout-header />
+    <layout-sidebar :openSidebar="isOpenMenu" />
+    <div :class="['content', { content_full: !isOpenMenu }]">
+      <router-view />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss">
+@import "./styles/global.scss";
+
+.content {
+  max-width: 1400px;
+  margin-left: 250px;
+  padding: 30px;
+  transition: 0.2s;
+  &_full {
+    margin-left: 0;
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.sidebar-toggle {
+  position: fixed;
+  left: 0;
+  width: 15px;
+  background: var(--primary);
+  height: 100%;
+  top: 62px;
+  z-index: 1;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+@media screen and (max-width: 1023px) {
+  .content {
+    margin-left: 0;
+  }
 }
 </style>
